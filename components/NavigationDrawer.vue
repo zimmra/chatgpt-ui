7e69ca2c8da26cd6ca8f31501a53ad0093224e18
Jian-Wei Zhang <zjw.math@qq.com>
2023-05-16 14:31:59 +0800

    update feedback link

--- components/NavigationDrawer.vue
+++ components/NavigationDrawer.vue
@@ -20,7 +20,7 @@ const setTheme = (theme) => {
   colorMode.preference = theme
 }
 const feedback = () => {
-  window.open('https://github.com/WongSaang/chatgpt-ui/issues', '_blank')
+  window.open('https://github.com/Jarvis73/chatgpt-ui/issues', '_blank')
 }
 
 const { locale, locales, setLocale } = useI18n()
