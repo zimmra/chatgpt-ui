518fd98354d6c085d7be604173081ccd41dec9da
FengHZ <630153601@qq.com>
2023-06-15 16:00:46 +0800

    currently pause azure

--- utils/enums.js
+++ utils/enums.js
@@ -6,16 +6,16 @@ export const STORAGE_KEY = {
 }
 
 export const MODELS = {
+    // 'gpt-3.5-turbo': {
+    //     name: 'gpt-3.5-turbo',
+    //     frequency_penalty: 0.0,
+    //     presence_penalty: 0.0,
+    //     total_tokens: 4096,
+    //     max_tokens: 1000,
+    //     temperature: 0.7,
+    //     top_p: 1.0
+    // },
     'gpt-3.5-turbo': {
-        name: 'gpt-3.5-turbo',
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-        total_tokens: 4096,
-        max_tokens: 1000,
-        temperature: 0.7,
-        top_p: 1.0
-    },
-    'gpt-3.5-turbo-0613': {
         name: 'gpt-3.5-turbo-0613',
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
