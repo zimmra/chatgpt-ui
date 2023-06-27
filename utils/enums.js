1743b06540d79addd2bbcde98aa524b0112d9ffb
FengHZ <fenghz@zju.edu.cn>
2023-06-27 20:02:44 +0800

    change 3.5  default params

--- utils/enums.js
+++ utils/enums.js
@@ -29,7 +29,7 @@ export const MODELS = {
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
         total_tokens: 16384,
-        max_tokens: 14384,
+        max_tokens: 14000,
         temperature: 1.0,
         top_p: 1.0
     },
@@ -38,7 +38,7 @@ export const MODELS = {
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
         total_tokens: 8192,
-        max_tokens: 2000,
+        max_tokens: 6000,
         temperature: 1.0,
         top_p: 1.0
     }
