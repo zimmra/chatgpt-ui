57d6f8f240759bcfa7cd7f8639370e312e3ab9b2
Zou <zouzijun1205@gmail.com>
2023-07-30 13:47:35 +0800

    conversation delete confirm

---
+++ components/ConvDeleteDialog.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-dialog v-model="localIsShow" persistent max-width="768">
+        <v-card>
+            <v-card-title>
+                <span class="text-h5">{{ $t('confirmDeleteConversation') }}</span>
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-card-text>
+                {{ $t('confirmDeleteConversationDetails') + pageTitle }}
+            </v-card-text>
+            <v-divider></v-divider>
+
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click="onConfirm()"> {{ $t('Confirm deletion') }} </v-btn>
+                <v-btn color="primary" @click="onCancel()"> {{ $t('Cancel deletion') }} </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script setup>
+const props = defineProps({
+    isShow: Boolean,
+    items: Object,
+    cIdx: Number,
+})
+const { isShow, items, cIdx } = toRefs(props)
+
+const localIsShow = ref(isShow.value)
+
+const pageTitle = computed(() => document.title)
+
+watch(isShow, (newValue) => {
+    localIsShow.value = newValue;
+});
+
+const emit = defineEmits([
+    'on:confirm', 'update:isShow'
+]);
+
+function onConfirm() {
+    emit('on:confirm', items.value, cIdx.value)
+    onCancel()
+}
+
+function onCancel() {
+    localIsShow.value = false
+    emit('update:isShow', localIsShow.value)
+}
+</script>
